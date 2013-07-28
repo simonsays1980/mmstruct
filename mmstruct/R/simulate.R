143a42a9f5170732b7da58c7e7f945ac78fd9259
simon.zehnder <simon.zehnder@googlemail.com>
2013-07-28 15:19:30 +0200

    Switched from Scythestat to RcppArmadillo. added a .gitignore.

--- mmstruct/R/simulate.R
+++ mmstruct/R/simulate.R
@@ -1,4 +1,4 @@
-"simulateEKOP" <- function(size = 1000, alpha = 0.2, epsilon = 1.3, delta = 0.5, mu = 0.02, T = 60 * 6.5) {
+"simulateEKOP" <- function(size = 1000, alpha = 0.2, epsilon = 0.2, delta = 0.5, mu = 0.02, T = 60 * 6.5) {
 			sample <- .Call("simulateEKOP_cc", as.integer(size), as.double(alpha), as.double(epsilon), 
 						as.double(delta), as.double(mu), as.double(T))
 			colnames <- c("Mis", "Buy", "Sell", "Trades")
@@ -8,7 +8,7 @@
 			return(sample)
 }
 
-"simulateEKOPMis" <- function(size = 1000, alpha = 0.2, epsilon = 1.3, delta = 0.5, mu = 0.02, T = 60 * 6.5, mis = 0.15) {
+"simulateEKOPMis" <- function(size = 1000, alpha = 0.2, epsilon = 0.2, delta = 0.5, mu = 0.02, T = 60 * 6.5, mis = 0.15) {
 			sample <- .Call("simulateEKOPMis_cc", as.integer(size), as.double(alpha), as.double(epsilon),
 					as.double(delta), as.double(mu), as.double(T), as.double(mis))
 		
