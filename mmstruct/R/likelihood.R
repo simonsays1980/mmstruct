1dce7c5422ec318767c3a490722ffb47783a08bf
Simon  Zehnder <simonzehnder@extern-251-192.vpn.uni-bonn.de>
2013-09-19 11:14:40 +0200

    Added original likelihood for EKOP model.

--- mmstruct/R/likelihood.R
+++ mmstruct/R/likelihood.R
@@ -31,45 +31,74 @@
 	return(likl)
 }
 
-"computeKokotLikGrad" <- function(data, par, T, methodLik = c("precise", "approx")) {
+"computeEKOPOrigLik" <- function(data, par, T, methodLik = c("precise", "approx"))
+{
+    ## Check if correct argument is given ##
+    method <- match.arg(methodLik)
+    
+    ## Transform parameters ##
+    lalpha      <- exp(par[1])/(1 + exp(par[1]))
+    ldelta      <- exp(par[3])/(1 + exp(par[3]))
+    lepsilon    <- par[2]
+    lmu         <- par[4]
 
-	## Check if correct argument is given ##
-	method <- match.arg(methodLik)
-	
-	grad 		<- c(0, 0, 0)
-	tradeter1	<- matrix(0, NROW(data), 1)
-	tradeter2 	<- matrix(0, NROW(data), 1)
-	
-	## Transform parameters ##
-	lalpha 		<- exp(par[1])/(1 + exp(par[1]))
-	lepsilon 	<- par[2]
-	lmu 		<- par[3]
-	
-	## Compute Terms for likelihood and derivatives ##
-	for(i in 1:NROW(data)) {
-		tradeter1[i] 	<- data[i] * log(2 * lepsilon * T) - lgamma(data[i] + 1)
-		tradeter2[i] 	<- data[i] * log((2 * lepsilon + lmu) * T) - lgamma(data[i] + 1) 
-	}
-	epster1 	<- (-2 * lepsilon * T)
-	epster2 	<- (-(2 * lepsilon + lmu) * T)
-	sum1		<- (1 - lalpha) * exp(epster1 + tradeter1)
-	sum2 		<- lalpha * exp(epster2 + tradeter2)
-	sum3 		<- data/(2 * lepsilon * T) - 1
-	sum4		<- data/((2 * lepsilon + lmu) * T) - 1
-	
-	likl 		<- sum((sum1 + sum2))
-	if(method == "approx") {
-		likl[likl == -Inf] <- -1e+6
-	}
-	
-	## Derivative towards lalpha ##
-	grad[1] 	<- sum(sum2 - sum1)
-	
-	## Derivative towards lepsilon ##
-	grad[2]		<- sum(sum1 * 2 * T * sum3 + sum2 * 2 * T * sum4)
-	
-	## Derivative towards lmu ##
-	grad[3]		<- sum(sum2 * T * sum4)
-	
-	return(grad / likl)
+    ## Data variables ##
+    B           <- data[, 3]
+    S           <- data[, 4]
+
+    ## Allocate containers to hold values ##
+    tradeter1   <- matrix(0, NROW(data), 1)
+    tradeter2   <- matrix(0, NROW(data), 1)
+    tradeter3   <- matrix(0, NROW(data), 1)
+    tradeter4   <- matrix(0, NROW(data), 1)
+
+    ## Compute likelihood ##
+    for (i in 1:NROW(data)) {
+        tradeter1[i]    <- B[i] * log(lepsilon * T) - lgamma(B[i] + 1)
+        tradeter2[i]    <- S[i] * log(lepsilon * T) - lgamma(S[i] + 1)
+        tradeter3[i]    <- B[i] + log((lepsilon + lmu) * T) - lgamma(B[i] + 1)
+        tradeter4[i]    <- S[i] + log((lepsilon + lmu) * T) - lgamma(S[i] + 1)
+    }
+
+    epster1     <- (-lepsilon * T) 
+    epster2     <- (-(lepsilon + lmu) * T)
+    sum1        <- (1 - lalpha) * exp(epster1 + tradeter1 + epster1 + tradeter2)
+    sum2        <- lalpha * ldelta * exp(epster1 + tradeter1 + epster2 + tradeter4)
+    sum3        <- lalpha * (1 - ldelta) * exp(epster2 + tradeter3 + epster1 + tradeter2)
+
+    likl        <- sum(log(sum1 + sum2 + sum3))
+    if (method == "approx") {
+        likl[likl == -Inf] <- -1e+6
+    }
+    return(likl)
 }
+
+"computeEKOPLik" <- function(data, par, T, methodLik = c("precise", "approx"))
+{
+    ## Check if correct argument is given ##
+    method <- match.arg(methodLik)
+
+    ## Transform parameters ##
+    lalpha      <- exp(par[1])/(1 + exp(par[1]))
+    ldelta      <- exp(par[3])/(1 + exp(par[3]))
+    lepsilon    <- par[2]
+    lmu         <- par[4]
+    x           <- lepsilon/(lepsilon + lmu)
+   
+    buys        <- data[,3]
+    sells       <- data[,4]
+    trades      <- data[,5]
+    M           <- as.matrix(apply(data[,3:4], 1, min) 
+                            + apply(data[, 3:4], 1, max)/2)
+
+    ## Compute Likelihood ##
+    ter1    <- (-2) * (lepsilon * T) + M * log(x) + trades * log((lmu + lepsilon) * T)
+    ter2    <- lalpha * (1 - ldelta) * exp((-1) * lmu * T) * x^(sells - M)
+    ter2    <- ter2 + lalpha * ldelta * exp((-1) * lmu * T) * x^(buys - M) + (1 - lalpha) * x^(trades - M)
+
+    likl    <- sum(ter1, na.rm = TRUE) + sum(log(ter2), na.rm = TRUE)
+    if (method == "approx") {
+        likl[likl == -Inf]  <- -1e+6
+    }
+    return(likl)
+    }
--- mmstruct/R/model_EKOP.R
+++ mmstruct/R/model_EKOP.R
@@ -2,18 +2,18 @@
 	
 	if (missing(startpar)) {
 		cat("Using default starting values...\n")
-		tmp <- mean(data, na.rm = TRUE)/T
+		tmp         <- mean(data, na.rm = TRUE)/T
 		startpar 	<- c(0, tmp * 0.75/2, tmp * 0.25/2)
 	}
 
 	## optimization settings ##
-	optim_fn 	<- computeKokotLik
+	optim_fn 	    <- computeKokotLik
 	optim_Method 	<- "L-BFGS-B"
 	optim_lower 	<- c(-1e+6, 0, 0)
 	optim_upper 	<- c(1e+6, 1e+6, 1e+6)
 	optim_fnscale 	<- -1 
 	optim_maxit 	<- 200
-	optim_ctrl	<- list(fnscale = optim_fnscale, maxit = optim_maxit)
+	optim_ctrl	    <- list(fnscale = optim_fnscale, maxit = optim_maxit)
 	 
 	optim_res <- optim(par = startpar, fn = optim_fn, data = data, T = T, 
 				methodLik = methodLik, method = optim_Method, 
@@ -21,3 +21,30 @@
 				control = optim_ctrl, hessian  = TRUE)
 	return(optim_res)	
 }
+
+"estModelEKOPML" <- function(data, startpar, T = 390, methodLik = c("precise", "approx"),
+                             sim = FALSE, mis = FALSE, mis.prob, misInd) {
+	
+	if (missing(startpar)) {
+		cat("Using default starting values...\n")
+		tmp         <- mean(data[, 5], na.rm = TRUE)/T
+		startpar 	<- c(0, tmp * 0.75/2, 0, tmp * 0.25/2)
+	}
+
+	## optimization settings ##
+	optim_fn 	    <- computeEKOPLik
+	optim_Method 	<- "L-BFGS-B"
+	optim_lower 	<- c(-1e+6, 0, -1e+6, 0)
+	optim_upper 	<- c(1e+6, 1e+6, 1e+6, 1e+6)
+	optim_fnscale 	<- -1 
+	optim_maxit 	<- 200
+	optim_ctrl	    <- list(fnscale = optim_fnscale, maxit = optim_maxit, trace = 6)
+	 
+	optim_res <- optim(par = startpar, fn = optim_fn, data = data, T = T, 
+				methodLik = methodLik, method = optim_Method, 
+				lower = optim_lower, upper = optim_upper, 
+				control = optim_ctrl, hessian  = TRUE)
+	return(optim_res)	
+}
+
+
